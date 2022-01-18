5720d905484fb0cf984f223791fd8525d8dc33dd
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-18 19:24:49 -0300

    Configurando banco de dados

---
+++ src/Configs/Database.js
@@ -0,0 +1,11 @@
+export default {
+  username: 'postgres',
+  password: '1234',
+  database: 'Stronda',
+  host: 'localhost',
+  dialect: 'postgres',
+  dialectOptions: {
+    useUTC: false,
+  },
+  timezone: '-03:00',
+};
