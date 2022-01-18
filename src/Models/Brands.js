fda4edebd29b965d58d4352db8cc647d1014823c
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-18 19:35:42 -0300

    Criando model de marcas

---
+++ src/Models/Brands.js
@@ -0,0 +1,27 @@
+import db from '../Configs/Sequelize';
+
+const { sequelize } = db;
+const { Model, DataTypes } = db.Sequelize;
+
+class Brands extends Model {}
+
+Brands.init(
+  {
+    Id: {
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+    },
+    Name: {
+      allowNull: false,
+      type: DataTypes.STRING(300),
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    tableName: 'Brands',
+  }
+);
+
+export default Brands;
