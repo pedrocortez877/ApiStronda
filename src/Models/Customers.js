f27052bd0403639025a795b016a605ee76371b23
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-18 19:46:39 -0300

    Criando model de clientes

---
+++ src/Models/Customers.js
@@ -0,0 +1,55 @@
+import db from '../Configs/Sequelize';
+
+import Adresses from './Adresses';
+
+const { sequelize } = db;
+const { Model, DataTypes } = db.Sequelize;
+
+class Customers extends Model {}
+
+Customers.init(
+  {
+    Id: {
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+    },
+    Name: {
+      allowNull: false,
+      type: DataTypes.STRING(200),
+    },
+    DocumentNumber: {
+      allowNull: false,
+      type: DataTypes.STRING(14),
+    },
+    DocumentType: {
+      allowNull: false,
+      type: DataTypes.STRING(14),
+    },
+    PhoneNumber: {
+      allowNull: false,
+      type: DataTypes.STRING(15),
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    tableName: 'Customers',
+  }
+);
+
+Customers.Adresses = Customers.hasOne(Adresses, {
+  foreignKey: {
+    name: 'IdAdress',
+    allowNull: false,
+  },
+});
+
+Adresses.Customers = Adresses.belongsTo(Customers, {
+  foreignKey: {
+    name: 'Id',
+    allowNull: false,
+  },
+});
+
+export default Customers;
