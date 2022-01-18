4be1ceb1e7dbb1ab3f22032884424f8db5853e50
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-18 20:16:10 -0300

    Criando model de fornecedores

---
+++ src/Models/Suppliers.js
@@ -0,0 +1,31 @@
+import db from '../Configs/Sequelize';
+
+const { sequelize } = db;
+const { Model, DataTypes } = db.Sequelize;
+
+class Suppliers extends Model {}
+
+Suppliers.init(
+  {
+    Id: {
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+    },
+    Name: {
+      allowNull: false,
+      type: DataTypes.STRING(300),
+    },
+    Cnpj: {
+      allowNull: false,
+      type: DataTypes.STRING(14),
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    tableName: 'Suppliers',
+  }
+);
+
+export default Suppliers;
