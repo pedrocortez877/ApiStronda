ef1d29447532c14d2df1bf63c9ef200829ad61f6
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-18 20:06:32 -0300

    Criando model de produtos

---
+++ src/Models/Products.js
@@ -0,0 +1,47 @@
+import db from '../Configs/Sequelize';
+
+import Brands from './Brands';
+
+const { sequelize } = db;
+const { Model, DataTypes } = db.Sequelize;
+
+class Products extends Model {}
+
+Products.init(
+  {
+    Id: {
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+    },
+    Name: {
+      allowNull: false,
+      type: DataTypes.STRING(200),
+    },
+    Description: {
+      allowNull: false,
+      type: DataTypes.STRING(200),
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    tableName: 'Products',
+  }
+);
+
+Products.Brands = Products.belongsTo(Brands, {
+  foreignKey: {
+    name: 'IdBrand',
+    allowNull: false,
+  },
+});
+
+Brands.Products = Brands.hasMany(Products, {
+  foreignKey: {
+    name: 'Id',
+    allowNull: false,
+  },
+});
+
+export default Products;
