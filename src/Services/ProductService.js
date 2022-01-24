99753aabdbe68bee5b0c194b486477f7c478b717
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-24 20:38:47 -0300

    Criando service de produtos

---
+++ src/Services/ProductService.js
@@ -0,0 +1,25 @@
+import ProductRepository from '../Repositories/ProductRepository.js';
+
+class ProductService {
+  async create(data) {
+    const product = await ProductRepository.create(data);
+    return product;
+  }
+
+  async read() {
+    const products = await ProductRepository.findAll();
+    return products;
+  }
+
+  async update(data) {
+    const updatedProduct = await ProductRepository.update(data);
+    return updatedProduct;
+  }
+
+  async delete(data) {
+    const deletedProduct = await ProductRepository.delete(data);
+    return deletedProduct;
+  }
+}
+
+export default new ProductService();
