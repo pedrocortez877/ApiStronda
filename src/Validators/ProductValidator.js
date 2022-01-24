c7cc29b5651489ab56440a4162f3de2c497e2096
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-24 20:56:43 -0300

    Criando validator de produto

---
+++ src/Validators/ProductValidator.js
@@ -0,0 +1,14 @@
+import yup from '../Helpers/Yup.js';
+
+export default (req, res, next) => {
+  const schema = yup.object().shape({
+    Name: yup.string().required(),
+    Description: yup.string().required(),
+    IdBrand: yup.number().min(1).required(),
+  });
+
+  return schema
+    .validate(req.body)
+    .then(() => next())
+    .catch(({ errors }) => res.status(400).json({ message: errors.pop() }));
+};
