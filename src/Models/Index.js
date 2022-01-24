9084f08f8ba40873abec2e4816f92dfc8b5bf9ab
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-24 10:48:12 -0300

    Criando index para abstração de models

---
+++ src/Models/Index.js
@@ -0,0 +1,29 @@
+import Adresses from './Adresses';
+import Brands from './Brands';
+import Customers from './Customers';
+import Products from './Products';
+import ProductSale from './ProductSale';
+import ProductsOfASale from './ProductsOfASale';
+import ProductsOfAService from './ProductsOfAService';
+import Purchases from './Purchases';
+import ServiceSale from './ServiceSale';
+import Stock from './Stock';
+import Suppliers from './Suppliers';
+import Users from './Users';
+
+const Models = [
+  Adresses,
+  Brands,
+  Customers,
+  Products,
+  ProductSale,
+  ProductsOfASale,
+  ProductsOfAService,
+  Purchases,
+  ServiceSale,
+  Stock,
+  Suppliers,
+  Users,
+];
+
+export default Models;
