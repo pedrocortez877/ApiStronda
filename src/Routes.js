440ef3832401039134636ce05ddfee3609d9e51f
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-24 21:08:16 -0300

    Adicionando rotas de produtos ao componente de rotas

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserRoute from './Routes/UserRoutes.js';
 import PingRoute from './Routes/PingRoutes.js';
+import ProductRoutes from './Routes/ProductRoutes.js';
 
 import Auth from './Middlewares/Auth.js';
 
@@ -12,5 +13,8 @@ routes.use('/ping', Auth, PingRoute);
 
 // ROTAS DE LOGIN
 routes.use('/login', UserRoute);
+
+// ROTAS DE PRODUTOS
+routes.use('/products', Auth, ProductRoutes);
 
 export default routes;
