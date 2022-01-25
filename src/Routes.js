c805b0ea0907836e2794b7c67c6f34b8abfb3518
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-24 21:53:05 -0300

    Adicionando rotas de marcas ao componente de rotas

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserRoute from './Routes/UserRoutes.js';
 import PingRoute from './Routes/PingRoutes.js';
 import ProductRoutes from './Routes/ProductRoutes.js';
+import BrandRoutes from './Routes/BrandRoutes.js';
 
 import Auth from './Middlewares/Auth.js';
 
@@ -16,5 +17,8 @@ routes.use('/login', UserRoute);
 
 // ROTAS DE PRODUTOS
 routes.use('/products', Auth, ProductRoutes);
+
+// ROTAS DE MARCAS
+routes.use('/brands', Auth, BrandRoutes);
 
 export default routes;
