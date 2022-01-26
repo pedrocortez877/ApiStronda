fe4540ab0352549bea3c5976310961229e8776ae
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-26 13:41:14 -0300

    Criando validator para insert de fornecedores

---
+++ src/Validators/Supplier/CreateSupplierValidator.js
@@ -0,0 +1,13 @@
+import yup from '../../Helpers/Yup.js';
+
+export default (req, res, next) => {
+  const schema = yup.object().shape({
+    Name: yup.string().required(),
+    Cnpj: yup.string().required().max(14),
+  });
+
+  return schema
+    .validate(req.body)
+    .then(() => next())
+    .catch(({ errors }) => res.status(400).json({ message: errors.pop() }));
+};
