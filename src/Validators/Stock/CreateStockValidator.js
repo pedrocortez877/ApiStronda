03a147a8c1894c99142ab269ea55d2d88378ee01
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-26 13:53:55 -0300

    Criando validator para insert de estoque

---
+++ src/Validators/Stock/CreateStockValidator.js
@@ -0,0 +1,14 @@
+import yup from '../../Helpers/Yup.js';
+
+export default (req, res, next) => {
+  const schema = yup.object().shape({
+    IdProduct: yup.number().required().min(1),
+    IdSuppler: yup.number().required().min(1),
+    Quantity: yup.number().required().min(1),
+  });
+
+  return schema
+    .validate(req.body)
+    .then(() => next())
+    .catch(({ errors }) => res.status(400).json({ message: errors.pop() }));
+};
