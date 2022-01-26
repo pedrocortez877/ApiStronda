cd598c30352a11e4d0c8ca4fe2911d69b4a5cd5f
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-26 12:00:15 -0300

    Adicionando rotas de vendas de produtos ao componente principal de rotas

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import ProductRoutes from './Routes/ProductRoutes.js';
 import BrandRoutes from './Routes/BrandRoutes.js';
 import AddressRoutes from './Routes/AddressRoutes.js';
 import CustomerRoutes from './Routes/CustomerRoutes.js';
+import ProductSaleRoutes from './Routes/ProductSaleRoutes.js';
 
 import Auth from './Middlewares/Auth.js';
 
@@ -28,5 +29,8 @@ routes.use('/addresses', Auth, AddressRoutes);
 
 // ROTAS DE CLIENTES
 routes.use('/customers', Auth, CustomerRoutes);
+
+// ROTAS DE VENDAS DE PRODUTOS
+routes.use('/productSales', Auth, ProductSaleRoutes);
 
 export default routes;
