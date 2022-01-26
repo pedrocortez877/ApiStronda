a1ca713225c4ab47e04909579c6ee1b5231a783b
Pedro Cortez <pedrocortez877@gmail.com>
2022-01-26 11:06:24 -0300

    Criando repositorio de clientes

---
+++ src/Repositories/CustomerRepository.js
@@ -0,0 +1,27 @@
+import Customers from '../Models/Customers.js';
+
+class CustomerRepository {
+  async create(data) {
+    const customer = await Customers.create(data);
+    return customer;
+  }
+
+  async read() {
+    const customer = await Customers.findAll();
+    return customer;
+  }
+
+  async update(data) {
+    const { Id } = data;
+    const updatedCustomer = await Customers.update(data, { where: { Id } });
+    return updatedCustomer;
+  }
+
+  async delete(data) {
+    const { Id } = data;
+    const deletedCustomer = await Customers.destroy({ where: { Id } });
+    return deletedCustomer;
+  }
+}
+
+export default new CustomerRepository();
