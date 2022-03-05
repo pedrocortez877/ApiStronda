9535205777a9ad13e0e0c8b2b2f2ce197d663ef8
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-05 17:26:28 -0300

    Criando repositorio de servicos de uma venda de servico

---
+++ src/Repositories/ServicesOfASaleServiceRepository.js
@@ -0,0 +1,34 @@
+import ServicesOfASaleService from '../Models/ServicesOfASaleService.js';
+
+class ServicesOfASaleServiceRepository {
+  async create(data) {
+    const servicesOfASaleService = await ServicesOfASaleService.create(data);
+    return servicesOfASaleService;
+  }
+
+  async read() {
+    const servicesOfASaleService = await ServicesOfASaleService.findAll();
+    return servicesOfASaleService;
+  }
+
+  async update(data) {
+    const { Id } = data;
+    const updatedServicesOfASaleService = await ServicesOfASaleService.update(
+      data,
+      {
+        where: { Id },
+      }
+    );
+    return updatedServicesOfASaleService;
+  }
+
+  async delete(data) {
+    const { Id } = data;
+    const deletedServicesOfASaleService = await ServicesOfASaleService.destroy({
+      where: { Id },
+    });
+    return deletedServicesOfASaleService;
+  }
+}
+
+export default new ServicesOfASaleServiceRepository();
