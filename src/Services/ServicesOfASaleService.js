5b7b3f3b0b9876189c9163a72ab48b6d1a84d116
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-05 17:26:07 -0300

    Criando service de servicos de uma venda de servico

---
+++ src/Services/ServicesOfASaleService.js
@@ -0,0 +1,29 @@
+import ServicesOfASaleServiceRepository from '../Repositories/ServicesOfASaleServiceRepository';
+
+class ServicesOfASaleService {
+  async create(data) {
+    const servicesOfASaleService =
+      await ServicesOfASaleServiceRepository.create(data);
+    return servicesOfASaleService;
+  }
+
+  async read() {
+    const servicesOfASaleService =
+      await ServicesOfASaleServiceRepository.read();
+    return servicesOfASaleService;
+  }
+
+  async update(data) {
+    const updatedServicesOfASaleService =
+      await ServicesOfASaleServiceRepository.update(data);
+    return updatedServicesOfASaleService;
+  }
+
+  async delete(data) {
+    const deletedServicesOfASaleService =
+      await ServicesOfASaleServiceRepository.delete(data);
+    return deletedServicesOfASaleService;
+  }
+}
+
+export default new ServicesOfASaleService();
