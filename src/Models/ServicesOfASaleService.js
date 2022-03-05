8f5e2d91f63f916301439d5c80dde34061186ed2
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-05 16:21:09 -0300

    Criando model de servicos de uma venda de servico

---
+++ src/Models/ServicesOfASaleService.js
@@ -0,0 +1,74 @@
+import db from '../Configs/Sequelize.js';
+
+import ServicesProvided from './ServicesProvided.js';
+import ServiceSale from './ServiceSale.js';
+
+const { sequelize } = db;
+const { Model, DataTypes } = db.Sequelize;
+
+class ServicesOfASaleService extends Model {}
+
+ServicesOfASaleService.init(
+  {
+    Id: {
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+    },
+    ServiceValue: {
+      allowNull: false,
+      type: DataTypes.DECIMAL,
+    },
+    DiscountPercentage: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+    },
+    DiscountValue: {
+      allowNull: false,
+      type: DataTypes.DECIMAL,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    tableName: 'ServicesOfASaleService',
+  }
+);
+
+ServicesProvided.ServicesOfASaleService = ServicesProvided.hasMany(
+  ServicesOfASaleService,
+  {
+    foreignKey: {
+      name: 'IdService',
+    },
+  }
+);
+
+ServicesOfASaleService.ServicesProvided = ServicesOfASaleService.belongsTo(
+  ServicesProvided,
+  {
+    foreignKey: {
+      name: 'IdService',
+    },
+  }
+);
+
+ServicesOfASaleService.ServiceSale = ServicesOfASaleService.belongsTo(
+  ServiceSale,
+  {
+    foreignKey: {
+      name: 'IdSale',
+    },
+  }
+);
+
+ServiceSale.ServicesOfASaleService = ServiceSale.hasOne(
+  ServicesOfASaleService,
+  {
+    foreignKey: {
+      name: 'IdSale',
+    },
+  }
+);
+
+export default ServicesOfASaleService;
