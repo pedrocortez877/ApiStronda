36bcab7234835751aedb2cd3cc026ea4b8e7656d
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-05 15:12:51 -0300

    Criando validacao para a venda de servicos

--- src/Validators/ServiceSale/CreateServiceSaleValidator.js
+++ src/Validators/ServiceSale/CreateServiceSaleValidator.js
@@ -1,15 +1,37 @@
 import yup from '../../Helpers/Yup.js';
 
-export default (req, res, next) => {
-  const schema = yup.object().shape({
-    IdCustomer: yup.number().min(1).required(),
-    ServiceSaleValue: yup.number().required(),
-    Invoice: yup.boolean(),
-    IdServiceProvided: yup.number().min(1).required(),
-  });
+export default async (req, res, next) => {
+  const schemaServiceSale = yup
+    .object()
+    .required()
+    .shape({
+      IdCustomer: yup.number().min(1).required(),
+      ServiceSaleValue: yup.number().required(),
+      Invoice: yup.boolean(),
+      IdServiceProvided: yup.number().min(1).required(),
+    });
 
-  return schema
-    .validate(req.body)
-    .then(() => next())
-    .catch(({ errors }) => res.status(400).json({ message: errors.pop() }));
+  const schemaProductsServiceSale = yup.array().of(
+    yup.object().shape({
+      Quantity: yup.number().required().min(1),
+      IdProduct: yup.number().required().min(1),
+      ProductValue: yup.number().required(),
+      DiscountPercentage: yup.number(),
+      DiscountValue: yup.number(),
+    })
+  );
+
+  try {
+    const validateServiceSale = await schemaServiceSale.validate(
+      req.body.ProductSale
+    );
+
+    const validateItemsServiceSale = await schemaProductsServiceSale.validate(
+      req.body.ItemsProductSale
+    );
+
+    if (validateServiceSale && validateItemsServiceSale) next();
+  } catch (e) {
+    return res.status(400).json({ message: e.errors.pop() });
+  }
 };
